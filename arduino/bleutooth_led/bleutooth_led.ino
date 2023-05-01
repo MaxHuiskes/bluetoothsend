4f46d9da69d89f60ab8db58ce74f17e9585fa6e8
MaxHuiskes <82757702+MaxHuiskes@users.noreply.github.com>
2023-05-01 16:38:27 +0200

    Update bleutooth_led.ino

--- arduino/bleutooth_led/bleutooth_led.ino
+++ arduino/bleutooth_led/bleutooth_led.ino
@@ -4,10 +4,13 @@ SoftwareSerial btm(2, 3);  // rx tx
 int index = 0;
 char data[10];
 char c;
+bool on = 0;
+int buzz = 0;
 boolean flag = false;
 void setup() {
   pinMode(LIGHT, OUTPUT);
   digitalWrite(LIGHT, LOW);
+  pinMode(BUZZER, OUTPUT);
   btm.begin(9600);
   Serial.begin(9600);
 }
@@ -25,6 +28,7 @@ void loop() {
   }
   if (flag) {
     processCommand();
+    buzzersound();
     flag = false;
     index = 0;
     data[0] = '\\0';
@@ -34,13 +38,20 @@ void processCommand() {
   char command = data[0];
   char inst = data[1];
   char exit = data[2];
-  if (inst == '1') {
+  if (inst == '3') {
+    on = !on;
+  }
+  if (inst == '1' || on) {
     digitalWrite(LIGHT, HIGH);
+    buzz = 1;
+    on = 1;
     btm.println("2");
-  } else if (inst == '2') {
+  } else if (inst == '2' || !on) {
     digitalWrite(LIGHT, LOW);
+    buzz = 0;
+    on = 0;
     btm.println("2");
-  } else{
+  } else {
     btm.println("5");
   }
 }
